f7374ba700691e70c08af7972a57e63759cf7b30
colemandorn <colemandorn@gmail.com>
2023-08-25 00:15:18 +0000

    cubbiesssss!

--- CUBBIES.R
+++ CUBBIES.R
@@ -16,3 +16,21 @@ hbp_plot <- ggplot(data = filter(cubs_batting, HBP > 0)) +
   labs(title = "Cubs Hit by Pitch", x = "Player") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+# Calculate batting average and create a new column 'BAvg'
+cubs_batting <- cubs_batting %>%
+  mutate(BAvg = H / AB)
+
+#calculate num of singles
+cubs_batting <- cubs_batting %>%
+  mutate(`1B` = H - `2B` - `3B` - HR)
+
+# Calculate WOBA w/ 8/24/23 cf
+cubs_batting <- cubs_batting %>%
+  mutate(WOBA = (((.696*BB)+(.726*HBP)+(.883*`1B`)+(1.245*`2B`)+(1.571*`3B`)+(2.007*HR))/(AB+BB-IBB+SF+HBP)))
+
+# WOBA - OBP
+cubs_batting <- cubs_batting %>%
+  mutate(`WOBA+-` = WOBA - OBP)
+
+
+
